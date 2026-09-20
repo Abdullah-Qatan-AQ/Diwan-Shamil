b950c09a411a597417435e0622b02dde847cbb9f
Abdullah Qatan <abdullahqatan560@gmail.com>
2026-09-20 11:31:57 +0000

    restore: return to latest PWA version

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const CACHE = "diwan-v27";
+const CACHE = "diwan-v29";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=27",
   "./app.js?v=27",
   "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
   "./icon-192.svg",
   "./icon-512.svg",
   "./data/quran.json",
